efa227974deb21f66f3324d23feb4842d60c76b5
Joakim-Jobloop <Joakim@jobloop.no>
2025-02-06 08:39:56 +0100

    *

--- app/page.js
+++ app/page.js
@@ -1,23 +1,25 @@
 import Image from "next/image";
 import { ClickButton } from "./components/button";
 
-
-
 export default function Home() {
-
-
   return (
-    <main className="grid gap-8 place-content-center min-h-screen">
+    <main className="grid min-h-screen gap-8 place-content-center">
       <h1>Next.js tutorial</h1>
       <div className="flex flex-wrap">
-      <figure className="h-[300px] w-[300px]">
-        <Image className="h-full w-full" src="/oldpc.png" width="1200" height="1200" alt="basketimage" />
-      </figure>
-      <figure className="h-[300px] w-[300px]">
-        <img className="h-full w-full" src="/oldpc.png" alt="basketimage" />
-      </figure>
+        <figure className="h-[300px] w-[300px]">
+          <Image
+            className="w-full h-full"
+            src="/oldpc.png"
+            width="1200"
+            height="1200"
+            alt="basketimage"
+          />
+        </figure>
+        <figure className="h-[300px] w-[300px]">
+          <img className="w-full h-full" src="/oldpc.png" alt="basketimage" />
+        </figure>
       </div>
-  <ClickButton/>
+      <ClickButton />
     </main>
   );
 }
